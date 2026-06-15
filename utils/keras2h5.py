804b3922fda7739422881f9f31118274bb725d07
Aaren Arasaratnam <aaren.aras@gmail.com>
2026-06-15 18:12:04 -0400

    Scrap To Do comment

--- utils/keras2h5.py
+++ utils/keras2h5.py
@@ -1,9 +1,6 @@
 import sys
 from pathlib import Path
 
-
-"""HOW TO GENERATE config.json and metadata.json as was done in earlier file version?"""
-
 ROOT_DIR = Path(__file__).resolve().parent.parent
 if str(ROOT_DIR) not in sys.path: sys.path.append(str(ROOT_DIR))
 
